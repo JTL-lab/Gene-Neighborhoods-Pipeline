bdacfd07cc37012b33b9f76eca82d8247754cfee
JTL-lab <jl228931@dal.ca>
2020-07-27 03:08:01 -0300

    Created setup.py, added directories, and begin setting up package

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+"""Exception block to modify later...
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from disutils.core import setup"""
+
+long_description="A bioinformatics pipeline designed to simplify the \
+      analysis of gene neighborhoods. Examines 5 genes \
+      upstream and 5 genes downstream from an annotated AMR gene, and provides\
+      cluster and phylogenetic visualization outputs from the identified \
+      gene neighborhoods."
+      
+setup(
+      name="namespace.gene_neighborhoods_pipeline",
+      version="0.1.0",
+      package_dir={"": "src"},
+      packages=find_namespace_packages(where="src")),
+      description="Bioinformatics pipeline to simplify analysis and \
+      visualization of gene neighborhoods."
+      long_description=long_description,
+      install_requires=["prokka>=1.14.5","rgi>=5.1.1","numpy>=1.19.0",
+                        "pandas>=1.0.5","biopython>=1.77","ete3>=3.1.1",
+                        "dendropy>=4.4.0"],
+      python_requires=">=3.5",
+      
+      #Things to modify later
+      author= "Chandana & Julia",
+      scripts="",
+      
+      
+)
